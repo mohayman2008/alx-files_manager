b83d842f625de9e38277ca044f6bca2568435e3d
Mohamed Ayman <mohayman2007@gmail.com>
2024-06-20 02:58:45 +0000

    0x04. Files manager - Task 4 -> 8 - Fix

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,9 +1,14 @@
 /* eslint-disable import/no-named-as-default */
+import sha1 from 'sha1';
 import { v4 as uuidv4 } from 'uuid';
+
+import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
+const SENCONDS_IN_DAY = 24 * 60 * 60;
+
 export default class AuthController {
-  static async getConnect(req, res) {
+  /* static async getConnect(req, res) {
     const { user } = req;
     const token = uuidv4();
 
@@ -13,6 +18,39 @@ export default class AuthController {
 
   static async getDisconnect(req, res) {
     const token = req.headers['x-token'];
+
+    await redisClient.del(`auth_${token}`);
+    res.status(204).send();
+  } */
+
+  static async getConnect(req, res) {
+    const authHeader = req.header('authorization');
+    if (!authHeader || authHeader.slice(0, 6) !== 'Basic ') {
+      res.status(401).json({ error: 'Unauthorized' });
+      return;
+    }
+    const b64EncodedAuth = authHeader.slice(6).trim();
+    const [email, password] = Buffer.from(b64EncodedAuth, 'base64').toString().split(/(?<!.*:.*):/);
+
+    const user = await dbClient.collectionFindOne('users', { email, password: sha1(password) });
+
+    if (!user) {
+      res.status(401).json({ error: 'Unauthorized' });
+      return;
+    }
+
+    const token = uuidv4();
+    await redisClient.set(`auth_${token}`, user._id.toString(), SENCONDS_IN_DAY);
+    res.status(200).json({ token });
+  }
+
+  static async getDisconnect(req, res) {
+    const token = (req.header('X-token') || '').trim();
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      res.status(401).json({ error: 'Unauthorized' });
+      return;
+    }
 
     await redisClient.del(`auth_${token}`);
     res.status(204).send();
